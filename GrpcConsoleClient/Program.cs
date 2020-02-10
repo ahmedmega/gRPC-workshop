77a6f2efc85098e0d9a9e5a45cdd960c7ee3bdba
Ahmed Megahed <mugorewish@hotmail.com>
2020-02-10 18:34:53 +0300

    Grpc first commit

---
+++ GrpcConsoleClient/Program.cs
@@ -0,0 +1,26 @@
+﻿
+//using Greet;
+using Grpc.Net.Client;
+using GrpcServiceNameSpace;
+using System;
+
+namespace GrpcConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var channel = GrpcChannel.ForAddress("https://localhost:5003"))
+            {
+
+                var client = new Greeter.GreeterClient(channel);
+                var reply = client.SayHello(
+                                  new HelloRequest { Name = "Ahmed" });
+                Console.WriteLine("Greeting: " + reply.Message);
+
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
